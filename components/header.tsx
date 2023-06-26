cf2cd254a17a0217068254edc0014a1d5c410b00
admin-at-519398779771 <eyesh0011@gmail.com>
2023-06-26 22:01:31 +0900

    20230626_header, recipies

--- components/header.tsx
+++ components/header.tsx
@@ -6,43 +6,45 @@ export default function Header(){
       <>
         <title>Tossi</title>
         <div className="position: fixed w-full h-28 px-14 bg-white">
-          <ul className='display: block pt-8 mx-auto'>
-            <li className='display: inline-block pr-96'>
-              <Image
-                  src="/logo_top.png"
-                  width={130}
-                  height={70}
-                  alt="This is flower."
-                />
-            </li>
-            <li className='display: inline-block pr-12'>
-              <Link href='/baking/tossiStory'>
-                <div className='hover:text-green-500 text-base'>Tossi Story</div>
+          <ul className="display: block pt-8 mx-auto">
+            <li className="display: inline-block pr-96 hover:cursor-pointer">
+              <Link legacyBehavior href="/">
+                <Image
+                      src="/logo_top.png"
+                      width={130}
+                      height={70}
+                      alt="This is flower."
+                    />
               </Link>
             </li>
-            <li className='display: inline-block pr-12'>
-              <Link href='/baking/recipes'>
-                <div className='hover:text-green-500 text-base'>Recipes</div>
+            <li className="display: inline-block pr-12">
+              <Link href="/baking/tossiStory">
+                <div className="text-base text-amber-700 hover:text-amber-300">Tossi Story</div>
               </Link>
             </li>
-            <li className='display: inline-block pr-12'>
-              <Link href='/baking/bakingStory'>
-                <div className='hover:text-green-500 text-base'>Baking Story</div>
+            <li className="display: inline-block pr-12">
+              <Link href="/baking/recipes">
+                <div className="text-base text-amber-700 hover:text-amber-300">Recipes</div>
               </Link>
             </li>
-            <li className='display: inline-block pr-12'>
-              <Link href='/baking/bakingJournal'>
-                <div className='hover:text-green-500 text-base'>Baking Journal</div>
+            <li className="display: inline-block pr-12">
+              <Link href="/baking/bakingStory">
+                <div className="text-base text-amber-700 hover:text-amber-300">Baking Story</div>
               </Link>
             </li>
-            <li className='display: inline-block pr-12'>
-              <Link href='/baking/notice'>
-                <div className='hover:text-green-500 text-base'>Login</div>
+            <li className="display: inline-block pr-12">
+              <Link href="/baking/bakingJournal">
+                <div className="text-base text-amber-700 hover:text-amber-300">Baking Journal</div>
               </Link>
             </li>
-            <li className='display: inline-block pr-12'>
-              <Link href='/baking/academy'>
-                <div className='hover:text-green-500 text-base'>Instagram</div>
+            <li className="display: inline-block pr-12">
+              <Link href="/baking/notice">
+                <div className="text-base text-amber-700 hover:text-amber-300">Login</div>
+              </Link>
+            </li>
+            <li className="display: inline-block pr-12">
+              <Link href="/baking/academy">
+                <div className="text-base text-amber-700 hover:text-amber-300">Instagram</div>
               </Link>
             </li>
           </ul>
--- pages/baking/recipes.tsx
+++ pages/baking/recipes.tsx
@@ -1,20 +1,20 @@
 export default function Recipes(){
   const Recipes = [
-    {"title": "StrawBerry Cake with Gluten Free", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake2", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake3", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake4", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake5", "img": "../img/victoriaCake_2.jpg"},
-    {"title": "cake6", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake7", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake8", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake9", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake10", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake11", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake12", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake13", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake14", "img": "../img/victoriaCake_2.jpg"}, 
-    {"title": "cake15", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
+    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg"}, 
   ];
   const rowCount = Math.ceil(Recipes.length / 3);
   const ROWS = 3;
@@ -22,11 +22,11 @@ export default function Recipes(){
     return (
       <div className="w-full h-3/4 px-14 pt-28 flex flex-col justify-center items-center space-y-10">
         <div className="w-full flex flex-col justify-center items-center mb-16">
-          <div className="py-6 text-3xl">RECIPES</div>
-          <div className="w-full h-full p-4 bg-slate-200 rounded-lg text-center">
+          <div className="py-6 text-3xl text-amber-700">RECIPES</div>
+          <div className="w-full h-full p-4 text-amber-600 bg-amber-200 rounded-lg text-center">
             <i className="text-lg">You are the butter to my bread, the breath to my life.</i>
             <br/>
-            <i className="text-sm">Julie and Julia</i>
+            <i className="text-sm">- Julie and Julia</i>
           </div>
         </div>
         <div className="flex flex-col space-y-10">
@@ -40,9 +40,10 @@ export default function Recipes(){
                   const recipe = Recipes[recipeIndex];
 
                   return (
-                    <div className="flex flex-col items-center space-y-2">
+                    <div className="flex flex-col items-center space-y-2 text-amber-800">
                       <div className="w-80 h-64 bg-[url('../img/victoriaCake_2.jpg')]"></div>
-                      <div className="text-base">{recipe.title}</div>
+                      <div className="font-extralight text-xl">{recipe.title}</div>
+                      <div className="font-light text-sm">WITH {recipe.ingredient}</div>
                     </div>
                   );
                 }
