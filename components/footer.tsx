16c8823ae225129604bd6b272326da31f06e0d1d
admin-at-519398779771 <eyesh0011@gmail.com>
2023-07-17 07:27:42 +0900

    20230717_flex to grid

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <div className="absolute px-40 w-full h-10 bottom-0 bg-amber-300">
+        <div className="absolute px-2 w-full h-10 bottom-0 bg-amber-300">
             <div className="text-sm	pt-2">copyright ©︎ All rights reserved</div>
         </div>
     )
--- pages/baking/recipes.tsx
+++ pages/baking/recipes.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Link from "next/link";
 
 interface Recipe {
+  id: number,
   title: string;
   ingredient: string;
   img: string;
@@ -10,27 +11,24 @@ interface Recipe {
 
 export default function Recipes(){
   const recipes: Recipe[]  = [
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"}, 
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},  
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"}, 
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},  
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
-    {"title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"} 
+    {"id": 0, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
+    {"id": 1, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
+    {"id": 2, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},
+    {"id": 3, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
+    {"id": 4, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
+    {"id": 5, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Moderate"},
+    {"id": 6, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},
+    {"id": 7, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"}, 
+    {"id": 8, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
+    {"id": 9, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},  
+    {"id": 10, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
+    {"id": 11, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"}, 
+    {"id": 12, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Advanced"},  
+    {"id": 13, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"},
+    {"id": 14, "title": "STRAWBERRY CAKE", "ingredient":"CONFECTIONER'S SUGAR", "img": "../img/victoriaCake_2.jpg", "level": "Easy"} 
   ];
 
-  const ROWCOUNT = Math.ceil(recipes.length / 3);
-  const ROWS = 3;
-
-  const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState("");
 
   const handleSelectChange = (event: any) => {
     const value = event.target.value;
@@ -66,33 +64,20 @@ export default function Recipes(){
             <option value="Advanced">Advanced</option>
           </select>
         </div>
-        <div className="flex flex-col space-y-10">
-          {Array(ROWCOUNT).fill("").map((_, rowIndex) => (
-            <div className="flex space-x-10" key={`row-${rowIndex}`}>
-              
-              {Array(ROWS).fill("").map((_, columnIndex) => {
-                const recipeIndex = rowIndex * ROWS + columnIndex;
-                if (recipeIndex < filteredRecipes.length) {
-                  const recipe = filteredRecipes[recipeIndex];
-
-                  return (
-                    <Link href={{ pathname: `/baking/${recipeIndex}` }}>                   
-                      <div className="flex flex-col items-center space-y-2 text-amber-800" key={`recipe-${recipeIndex}`}>
-                        <div className="w-80 h-64 bg-[url('../img/victoriaCake_2.jpg')]"></div>
-                        <i className="text-xs text-black self-end">level: {recipe.level}</i>
-                        <div className="font-extralight text-xl">{recipe.title}</div>
-                        <div className="font-light text-sm">WITH {recipe.ingredient}</div>
-                      </div>
-                    </Link>
-                  );
-                }
-              })}
-            </div>
+        <div className="grid grid-cols-1 xl:grid-cols-3 mt-2 gap-3">
+        {filteredRecipes.map((recipe, index) => (
+        <Link href={{ pathname: `/baking/${recipe.id}` }}>                   
+          <div className="flex flex-col items-center space-y-2 text-amber-800" key={`recipe-${recipe.id}`}>
+            <div className="w-80 h-64 bg-[url('../img/victoriaCake_2.jpg')]"></div>
+            <i className="text-xs text-black self-end">level: {recipe.level}</i>
+            <div className="text-base sm:text-xl font-extralight ">{recipe.title}</div>
+            <div className="text-xs sm:text-sm font-light">WITH {recipe.ingredient}</div>
+          </div>
+        </Link>
           ))}
         </div>
         <div className="h-20 w-full">
-              {/** blank */}
+              {/** TODO: paging */}
         </div>
       </div>
-    )
-}
+    )}
