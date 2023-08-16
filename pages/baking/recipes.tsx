38a370a09388c4fd5f1d45b8756c1112a617fdb8
admin-at-519398779771 <eyesh0011@gmail.com>
2023-08-16 13:19:11 +0900

    20230816_resolve build error

--- pages/baking/recipes.tsx
+++ pages/baking/recipes.tsx
@@ -4,26 +4,27 @@ import Recipe from '@/models/Recipe';
 import Image from "next/image";
 
 export default function Recipes(){
-  const recipes: Recipe[]  = [
-    {"id": 0, "title": "비건 초콜릿 코코넛 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/ChocolateCoconutCookie.jpg", "level": "Easy"},
-    {"id": 1, "title": "비건 글루텐 프리 오트밀 초콜릿 칩 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/oatmeal.jpg", "level": "Moderate"},
-    {"id": 2, "title": "비건 글루텐 프리 그래놀라 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/granola.jpg", "level": "Advanced"},
-    {"id": 3, "title": "비건 더블 로투스 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rottusCookie.jpg", "level": "Moderate"},
-    {"id": 4, "title": "비건 오레오 청크 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/oreoCookie.jpg", "level": "Moderate"},
-    {"id": 5, "title": "비건 라즈베리 콩포트 초콜릿 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/congpoteChocolateCookie.jpg", "level": "Moderate"},
-    {"id": 6, "title": "라즈베리 콩포트", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/congpoteChocolateCookie.jpg", "level": "Moderate"},
-    {"id": 7, "title": "비건 초코 크랙 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/chocoCrackCookie.jpg", "level": "Advanced"},
-    {"id": 8, "title": "비건 피넛 버터 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/peanutCookie.jpg", "level": "Advanced"}, 
-    {"id": 9, "title": "라즈베리 화이트 초콜릿 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rasberryWhiteChocolateCookie.jpg", "level": "Easy"},
-    {"id": 10, "title": "비건 르뱅 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rebangCookie.jpg", "level": "Advanced"},  
-    {"id": 11, "title": "비건 초코 케이크 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/chocoCakeScorn.jpg", "level": "Easy"},
-    {"id": 12, "title": "비건 글루텐 프리 비건 코코넛 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/coconutScorn.jpg", "level": "Easy"}, 
-    {"id": 13, "title": "비건 글루텐 프리 바나나 블루베리 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/bananaBlueberryScorn.jpg", "level": "Advanced"},  
-    {"id": 14, "title": "비건 글루텐 프리 초코칩 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/chocochipScorn.jpg", "level": "Easy"},
-    {"id": 15, "title": "비건 글루텐 프리 블루베리 머핀", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/bluberryMurfin.jpg", "level": "Easy"},
-    {"id": 16, "title": "비건 글루텐 프리 라즈베리 머핀", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rasberrymurfin.jpg", "level": "Easy"},
-    {"id": 17, "title": "비건 글루텐 프리 바나나 초콜릿 머핀", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/bananaChocolatemurfin.jpg", "level": "Easy"}, 
-  ];
+  // const recipes: Recipe[]  = [
+  //   {"id": 0, "title": "비건 초콜릿 코코넛 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/ChocolateCoconutCookie.jpg", "level": "Easy"},
+  //   {"id": 1, "title": "비건 글루텐 프리 오트밀 초콜릿 칩 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/oatmeal.jpg", "level": "Moderate"},
+  //   {"id": 2, "title": "비건 글루텐 프리 그래놀라 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/granola.jpg", "level": "Advanced"},
+  //   {"id": 3, "title": "비건 더블 로투스 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rottusCookie.jpg", "level": "Moderate"},
+  //   {"id": 4, "title": "비건 오레오 청크 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/oreoCookie.jpg", "level": "Moderate"},
+  //   {"id": 5, "title": "비건 라즈베리 콩포트 초콜릿 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/congpoteChocolateCookie.jpg", "level": "Moderate"},
+  //   {"id": 6, "title": "라즈베리 콩포트", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/congpoteChocolateCookie.jpg", "level": "Moderate"},
+  //   {"id": 7, "title": "비건 초코 크랙 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/chocoCrackCookie.jpg", "level": "Advanced"},
+  //   {"id": 8, "title": "비건 피넛 버터 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/peanutCookie.jpg", "level": "Advanced"}, 
+  //   {"id": 9, "title": "라즈베리 화이트 초콜릿 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rasberryWhiteChocolateCookie.jpg", "level": "Easy"},
+  //   {"id": 10, "title": "비건 르뱅 쿠키", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rebangCookie.jpg", "level": "Advanced"},  
+  //   {"id": 11, "title": "비건 초코 케이크 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/chocoCakeScorn.jpg", "level": "Easy"},
+  //   {"id": 12, "title": "비건 글루텐 프리 비건 코코넛 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/coconutScorn.jpg", "level": "Easy"}, 
+  //   {"id": 13, "title": "비건 글루텐 프리 바나나 블루베리 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/bananaBlueberryScorn.jpg", "level": "Advanced"},  
+  //   {"id": 14, "title": "비건 글루텐 프리 초코칩 스콘", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/chocochipScorn.jpg", "level": "Easy"},
+  //   {"id": 15, "title": "비건 글루텐 프리 블루베리 머핀", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/bluberryMurfin.jpg", "level": "Easy"},
+  //   {"id": 16, "title": "비건 글루텐 프리 라즈베리 머핀", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/rasberrymurfin.jpg", "level": "Easy"},
+  //   {"id": 17, "title": "비건 글루텐 프리 바나나 초콜릿 머핀", "img": "https://tossibaking.s3.ap-northeast-2.amazonaws.com/bananaChocolatemurfin.jpg", "level": "Easy"}, 
+  // ];
+  const recipes: Recipe[]  = [];
 
   const [selectedValue, setSelectedValue] = useState("");
 
