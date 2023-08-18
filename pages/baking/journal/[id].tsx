c922a6487d5efa632f1978a8f2b8b2f2236bd034
admin-at-519398779771 <eyesh0011@gmail.com>
2023-08-18 11:09:10 +0900

    20230818_get journal title

--- pages/baking/journal/[id].tsx
+++ pages/baking/journal/[id].tsx
@@ -1,4 +1,3 @@
-import JournalDetail from "@/models/JournalDetail";
 import RichTextRenderer from "@/styles/RichTextRenderer";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -7,34 +6,42 @@ import { useState, useEffect } from 'react';
 export default function Detail() {
   const router = useRouter();
   const [blocks, setBlocks] = useState<any[]>([]);
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(`/api/blocks/${router.query.id}`);
+        const response = await fetch(`/api/pages/${router.query.id}`);
+
         const responseData = await response.json();
-        // const updatedJournals = responseData.results
-        // .filter((result: any) => (
-        //   result.properties.title.title[0]?.plain_text && result.properties.image.url
-        // ))
-        // .map((result: any) => (
-        //   {
-        //   id: result.id,
-        //   title: result.properties.title.title[0].plain_text,
-        //   img: result.properties.image.url,
-        // }));
-        setBlocks(responseData.results);
+        setTitle(responseData.properties.title.title[0].plain_text);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching title data:', error);
       }
     }
     fetchData();
   }, [router.query.id]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`/api/blocks/${router.query.id}`);
+
+        const responseData = await response.json();
+        setBlocks(responseData.results);
+      } catch (error) {
+        console.error('Error fetching block data:', error);
+      }
+    }
+    fetchData();
+  }, [router.query.id]);
+
+
+
   return (
     <div className="w-full h-full px-14 py-28 flex flex-col justify-center items-center space-y-10">
-      <div id="test" className="h-1/4 flex flex-col justify-center items-center text-center space-y-3">
-        {/* <RichTextRenderer richTextData={journal?.title} /> */}
+      <div id="test" className="h-1/4 text-3xl flex flex-col justify-center items-center text-center space-y-3">
+        {title}
       </div>
       <div className="h-5/6 pb-20 text-base tracking-wide leading-8">
         {blocks.map((block, index) => (
@@ -42,12 +49,12 @@ export default function Detail() {
           {block.type === "image" ?
           <div className="pb-10 flex flex-col justify-center items-center">
             <Image
-            src={block.image.external.url}
-            alt="This is journal"
-            priority
-            width={700}
-            height={400}
-          /> 
+              src={block.image.external.url}
+              alt="This is journal"
+              priority
+              width={700}
+              height={400}
+            /> 
           </div>
         : 
           <RichTextRenderer key={index} richTextData={block} /> }
