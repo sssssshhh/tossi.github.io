c7165bdf324ac5fea6c0d1cc7a7d2f533a33872b
admin-at-519398779771 <eyesh0011@gmail.com>
2023-08-21 06:32:48 +0900

    20230821_check deploy

--- pages/baking/story/[id].tsx
+++ pages/baking/story/[id].tsx
@@ -1,7 +1,7 @@
 import RichTextRenderer from "@/styles/RichTextRenderer";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function Detail() {
   const router = useRouter();
