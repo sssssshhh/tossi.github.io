9918c8ccc5357a7f58439b484d05e2ed1df74b51
admin-at-519398779771 <eyesh0011@gmail.com>
2023-09-01 07:51:51 +0900

    20230901_build test

--- pages/api/DB/recipe.js
+++ pages/api/DB/recipe.js
@@ -1,19 +1,21 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const notionApiUrl = `https://api.notion.com/v1/databases/1b83168b8cec4490abfe757fe53936bf/query`;
-
-  // const notionApiUrl = `https://api.notion.com/v1/databases/${process.env.NEXT_PUBLIC_NOTION_DB_RECIPE_ID}/query`;
-//       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_NOTION_KEY}`,
+  try {
+  const notionApiUrl = `https://api.notion.com/v1/databases/${process.env.NEXT_PUBLIC_NOTION_DB_RECIPE_ID}/query`;
 
   const response = await fetch(notionApiUrl, {
     method: 'POST',
     headers: {
-      'Authorization': `Bearer secret_fWFtpdlz4LYHtsPSu09EladKSiPfFQkK8LkuDyguZJj`,
-      'Notion-Version': '2022-06-28',
+       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_NOTION_KEY}`,
+       'Notion-Version': '2022-06-28',
     },
   });
  
   const data = await response.json();
   res.status(response.status).json(data);
+  } catch (error) {
+    console.error('Error fetching recipe data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
