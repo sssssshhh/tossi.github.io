a5f39a81ac8801dc39bad1988a4a4e753f237e65
admin-at-519398779771 <eyesh0011@gmail.com>
2023-09-01 07:27:14 +0900

    20230901_error test

---
+++ pages/test.tsx
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+export default async function  Test(){  
+  const notionApiUrl = `https://api.notion.com/v1/databases/1b83168b8cec4490abfe757fe53936bf/query`;
+
+  // const notionApiUrl = `https://api.notion.com/v1/databases/${process.env.NEXT_PUBLIC_NOTION_DB_RECIPE_ID}/query`;
+//       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_NOTION_KEY}`,
+
+  const response = await fetch(notionApiUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer secret_fWFtpdlz4LYHtsPSu09EladKSiPfFQkK8LkuDyguZJj`,
+      'Notion-Version': '2022-06-28',
+    },
+  });
+ 
+  const data = await response.json();
+  console.log(data);
+}
