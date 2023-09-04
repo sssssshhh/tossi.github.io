672ef03e469c83d1dbb64f26034b5266e07f0f4b
admin-at-519398779771 <eyesh0011@gmail.com>
2023-09-05 07:19:42 +0900

    20230905_build test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useRouter } from "next/router";
-import Footer from '@/components/footer'
-import Header from '@/components/header';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <div className="w-full min-h-screen relative">
-      <Header/>
+    
       <div className="w-full h-4/6 xl:h-2/6 pt-3 px-10 md:px-24">
         <Component {...pageProps} />
       </div>
-      <Footer/>
+ 
     </div>
   )
 }
