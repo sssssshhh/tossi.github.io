870155f2b760078cd12ff1e1d047782c707d03c2
admin-at-519398779771 <eyesh0011@gmail.com>
2023-09-09 22:53:36 +0900

    20230909_static journal

--- pages/baking/journal/mydata.js
+++ pages/baking/journal/mydata.js
@@ -1,13 +1,4 @@
-import RichTextRenderer from "@/styles/RichTextRenderer";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { useState, useEffect } from 'react';
-
-export default function Detail() {
-  const router = useRouter();
-  const [blocks, setBlocks] = useState<any[]>([]);
-  const [title, setTitle] = useState("");
-  const myData = [
+const myData = [
     {
         "title": "빵을 먹으면 몸에 안 좋을까요?",
         "object": "list",
@@ -2305,65 +2296,3 @@ export default function Detail() {
         "block": {}
     }
 ]
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        // const response = await fetch(`/api/pages/${router.query.id}`);
-        // const responseData = await response.json();
-
-        const responseData2 = myData.find(result => 
-            result.results[0].parent.page_id = String(router.query.id)
-          );
-
-        if(responseData2) {
-            setTitle(responseData2.title);
-            setBlocks(responseData2.results);
-        }
-      } catch (error) {
-        console.error('Error fetching title data:', error);
-      }
-    }
-    fetchData();
-  }, [router.query.id]);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(`/api/blocks/${router.query.id}`);
-
-        const responseData = await response.json();
-        setBlocks(responseData.results);
-      } catch (error) {
-        console.error('Error fetching block data:', error);
-      }
-    }
-    fetchData();
-  }, [router.query.id]);
-
-  return (
-    <div className="w-full h-full px-14 py-28 flex flex-col justify-center items-center space-y-10">
-      <div className="h-1/4 text-3xl flex flex-col justify-center items-center text-center space-y-3">
-        {title}
-      </div>
-      <div className="h-5/6 pb-20 text-base tracking-wide leading-8">
-        {blocks.map((block, index) => (
-        <>
-          {block.type === "image" ?
-          <div className="pb-10 flex flex-col justify-center items-center">
-            <Image
-              src={block.image.external.url}
-              alt="This is journal"
-              priority
-              width={700}
-              height={400}
-            /> 
-          </div>
-        : 
-          <RichTextRenderer key={index} richTextData={block} /> }
-        </>
-        ))}
-      </div>
-    </div>
-  )
-}
